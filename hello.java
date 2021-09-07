e0dd4f13dd01cb1ed4aa23419177052516ff67f7
Omjai-Sutar <61621367+Omjai-Sutar@users.noreply.github.com>
2021-09-07 04:44:42 -0700

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,12 @@
+/**
+ * hello
+ */
+public class hello {
+
+    public static void main(String[] args) {
+      System.out.println("Hello Pranali.......");  
+      String x="Pranali";
+      System.out.println(x);
+    }
+
+}
